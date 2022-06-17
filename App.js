3ef8915094d7f5f7a95b280eda4125bf790f92c4
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-17 11:23:47 +0200

    fda

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar, Text, View } from 'react-native';
+import tw from 'twrnc'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`android:pt-[${StatusBar.currentHeight}px]`}>
+      <Text>Open up App.js to start workinddfg on your app!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
